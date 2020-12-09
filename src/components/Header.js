3b12ea85bfecdc568d4ff5643b7c0a8dd1585f20
Art Ianuzzi <art@ianuzzi.us>
2020-12-09 16:51:06 -0500

    Updated readme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,9 @@
-import { Icon } from '@iconify/react';
-import locationIcon from '@iconify/icons-mdi/fire';
-
 const Header = () => {
 	return (
 		<header className="header">
-			<h1>
-				<Icon icon={locationIcon} />
-				Wildfire Tracker (Powered by NASA)
-			</h1>
+			<h1>Event Tracker (Powered by NASA)</h1>
 		</header>
-	);
-};
+	)
+}
 
-export default Header;
+export default Header
